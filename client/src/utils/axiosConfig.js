c77c8ea6c85431dd6390875ae03936063bc81427
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2023-05-18 20:30:31 +0100

    commit

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_PUBLIC_API_URL);
+console.log(process.env.REACT_APP_API_URL);
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_PUBLIC_API_URL || 'http://localhost:3000/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/',
 });
 
 export default axiosInstance;
