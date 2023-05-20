a5b362c7dcf18fc4f393cab9627a5ef46b0d4c5a
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2023-05-20 20:10:55 +0100

    commit

--- server/src/server.js
+++ server/src/server.js
@@ -159,7 +159,7 @@ app.post('/api/user/login', async (req, res) => {
 
     const domain = BASE_URL == 'http://localhost:3000/' ? 'localhost' : '.vercel.app' 
     // Set the token as an HTTP-only cookie
-    res.cookie('token', token, { httpOnly: true, domain: '.onrender.com' });
+    res.cookie('token', token, { httpOnly: true, domain: '.onrender.com', sameSite: 'none' });
 
     res.json({ message: 'Login successful', user });
   } catch (error) {
